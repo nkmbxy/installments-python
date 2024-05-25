71800fb77655c88671773b662f8ffaf27e024c90
nkmbxy <nakamon22547@gmail.com>
2024-05-25 21:23:23 +0700

    edit work

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-
-from flask import Flask, request , jsonify
+from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
+
 app = Flask(__name__)
 
 knn_model = joblib.load('knn_model.pkl')
 scaler = joblib.load('scaler.pkl')
 label_encoders = joblib.load('label_encoders.pkl')
 
-@app.route('/predict-credit-scrolling', methods=['POST'])
+@app.route('/predict-credit-scoring', methods=['POST'])
 def predict():
     data = request.get_json()
     print(data)
     input_data = pd.DataFrame([data])
+
+    # Transform 'Marital Status' column only
     input_data['Marital Status'] = label_encoders['Marital Status'].transform(input_data['Marital Status'])
     input_data = scaler.transform(input_data)
+
     prediction = knn_model.predict(input_data)
     prediction_label = label_encoders['Credit Score'].inverse_transform(prediction)
     print(prediction_label)
-    
+
     return jsonify({'creditScore': prediction_label[0]})
+
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -6,33 +6,28 @@ import joblib
 
 data = pd.read_csv('Credit_Score_Classification_Dataset.csv')
 
-data = data.drop(columns=['Education', 'Home Ownership'])
-
+# Drop unnecessary columns including 'Gender'
+data = data.drop(columns=['Education', 'Home Ownership', 'Gender'])
 
 label_encoders = {}
-categorical_columns = [ 'Marital Status', 'Credit Score']
+categorical_columns = ['Marital Status', 'Credit Score']
 
 for column in categorical_columns:
     le = LabelEncoder()
     data[column] = le.fit_transform(data[column])
     label_encoders[column] = le
 
-
 X = data.drop(columns=['Credit Score'])
 y = data['Credit Score']
 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
-
 
 y_pred = knn.predict(X_test)
 
